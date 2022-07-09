a452f1e8ccbf151e65d1dda0ac8ce2789812871a
TofikSaliba <tofik.saliba@gmail.com>
2022-07-09 23:55:47 +0300

    spinner for map working, make sure its loaded

--- client/src/pages/map/Map.tsx
+++ client/src/pages/map/Map.tsx
@@ -3,23 +3,33 @@ import { usePreferences } from "../../contexts/Preferences.context";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 function Map() {
-  const { setIsLoading } = usePreferences();
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, [setIsLoading]);
-
+  const { setIsLoading, isLoading } = usePreferences();
+  const center = useMemo(() => ({ lat: 32.799, lng: 35 }), []);
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP!,
   });
 
-  if (!isLoaded) return <div>Loading...</div>;
+  useEffect(() => {
+    setIsLoading(true);
+    if (isLoaded) {
+      setIsLoading(false);
+    }
+  }, [isLoaded, setIsLoading]);
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
+    <>
+      {!isLoading && isLoaded && (
+        <div className="map-wrapper">
+          <GoogleMap
+            zoom={14}
+            center={center}
+            mapContainerClassName="map-container"
+          >
+            <Marker position={center} />
+          </GoogleMap>
+        </div>
+      )}
+    </>
   );
 }
 
