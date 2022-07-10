16b169a37f63cc09a9756f1b7c1260fe41e96c89
TofikSaliba <tofik.saliba@gmail.com>
2022-07-10 18:13:35 +0300

    chat working between 2, only need to provide id to send to

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,25 +1,31 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useSocket } from "../../contexts/Socket.context";
+import { useUser } from "../../contexts/User.context";
 
 function Home() {
   const inputRef: any = useRef();
   const { sendMessage, socket } = useSocket();
+  const { currentUser } = useUser();
   const [msg, setMsg] = useState("");
 
   const fetch = async () => {
-    sendMessage("62cae0339561dbdfaf1064d2", `${inputRef.current.value} from: `);
+    const to =
+      currentUser?._id === "62cae0339561dbdfaf1064d2"
+        ? "62c8b45b7f653b187994072f"
+        : "62cae0339561dbdfaf1064d2";
+    sendMessage(to, `${inputRef.current.value} from: `);
     inputRef.current.value = "";
   };
 
-  const getMessage = () => {
-    if (!socket) return;
-    socket.off("receiveMessage");
-    socket.on("receiveMessage", ({ msg }: any) => {
-      setMsg(msg);
-    });
-  };
-
-  getMessage();
+  useEffect(() => {
+    if (socket) {
+      // socket.off("receiveMessage");
+      socket.on("receiveMessage", ({ msg }: any) => {
+        setMsg(msg);
+      });
+      return () => socket.off("receiveMessage");
+    }
+  }, [socket]);
 
   return (
     <div>
